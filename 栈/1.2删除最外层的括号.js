10d227ab830bdb904dc442cfdcc7802aa31dc8bc
JSK427 <1484710039@qq.com>
2022-01-02 17:38:11 +0800

    1.2删除最外层的括号

--- 栈/1.2删除最外层的括号.js
+++ 栈/1.2删除最外层的括号.js
@@ -3,6 +3,8 @@
  * * 题目地址：https://leetcode-cn.com/problems/remove-outermost-parentheses
  */
 
+//利用栈的思路
+
 // * 思路：从头到尾寻找有效括号组合，push 到结果集中
 /**
  * @param {string} S
@@ -43,3 +45,22 @@ let test = "(()())(())";
 console.time("执行用时");
 console.log(removeOuterParentheses(test));
 console.timeEnd("执行用时");
+
+//常规思路
+/* 
+通过计数器找到需要去除的外层括号，留下有效符号
+*/
+
+var removeOuterParentheses = function(s) {
+    let count = 0,
+        ans = "";
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(" && count++ > 0) {
+            ans = ans + "(";
+        }
+        if (s[i] === ")" && count-- > 1) {
+            ans = ans + ")";
+        }
+    }
+    return ans;
+};
